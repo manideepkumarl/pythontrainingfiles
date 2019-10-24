db8f0d661e573c2303d27ff5f5f5a85abb8e49af
manideepkumarl <56963267+manideepkumarl@users.noreply.github.com>
2019-10-24 17:40:31 +0530

    Add files via upload

---
+++ exerise2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 15:22:59 2019
+
+@author: manideepl
+"""
+
+inputstring = input("Enter any delimanated string")
+inputlist = inputstring.split(",")
+print("List elements are:", inputlist)
+print("Length of the list: ", len(inputlist)) 
